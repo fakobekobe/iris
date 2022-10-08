5f587541a71e67d095933ec54105f97cc255a826
Tano Fabien <kobejeanfabien@gmail.com>
2022-10-08 12:55:42 +0000

    Ajout de dj-database-url

--- projet/prod_settings.py
+++ projet/prod_settings.py
@@ -1,6 +1,9 @@
+import dj_database_url
 from .settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['app-chlohe-iris.herokuapp.com']
 TEMPLATE_DEBUG = False
 SECRET_KEY = 'siqe7u-yaj$v*=p-4h#xpbo@e1gs)_ufwz88i^925fndi3!*kd'
+
+DATABASES['default'] = dj_database_url.config()
