62c749721717b0263c7b44ef3e3cbc2387ee0006
Tano Fabien <kobejeanfabien@gmail.com>
2022-10-08 14:22:00 +0000

    modification de urls

--- projet/urls.py
+++ projet/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     #path('',include('presentation.urls')),
     path('',include('utilisateur.urls')),
     # path('utilisateur/',include('utilisateur.urls')),
-] #+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
