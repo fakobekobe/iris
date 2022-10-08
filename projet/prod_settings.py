5266ddefac576b0abe226617361abb3ca8672aa6
Tano Fabien <kobejeanfabien@gmail.com>
2022-10-08 13:43:19 +0000

    Modification du fichier prod_settings pour ajouter le whitenoise

--- projet/prod_settings.py
+++ projet/prod_settings.py
@@ -7,5 +7,14 @@ TEMPLATE_DEBUG = False
 
 DATABASES['default'] = dj_database_url.config()
 
-MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
