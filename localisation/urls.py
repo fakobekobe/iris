25cc75f1ae1882e978193899e27390f6f68d5506
Tano Fabien <kobejeanfabien@gmail.com>
2022-10-17 12:21:09 +0000

    Ajout du pop up ajouter_district

--- localisation/urls.py
+++ localisation/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 app_name = 'localisation'
 urlpatterns = [
-    path('', views.district, name = 'district'),
+    path('', views.localisation, name = 'localisation'),
+    # Url du district
+    path('ajouter-district', views.modifier_district, name = 'ajouter_district'),
+    path('details-district/<str:id>', views.modifier_district, name = 'details_district'),
+    path('modifier-district', views.modifier_district, name = 'modifier_district'),
+    path('supprimer-district/<str:id>', views.supprimer_district, name = 'supprimer_district'),
 ]
--- localisation/views.py
+++ localisation/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 from .models import *
 
-def district(request):
+def localisation(request):
 
     districts = District.objects.all().order_by("id")
 
@@ -10,3 +12,67 @@ def district(request):
         "districts" : districts,
     }
     return render(request, "localisation/localisation.html", context)
+
+# Gestion du district -----------------------------------------------
+
+def ajouter_district(request):
+
+    context = {
+        "titre": "Localisation",
+    }
+
+    if request.method == "POST":
+        if request.POST.get("code") and request.POST.get("libelle"):
+            # On vérifie si le code et le libelle existe
+
+            #----
+            district = District()
+            district.code = request.POST.get("code")
+            district.libelle = request.POST.get("libelle")
+            district.save()
+
+            messages.success(request,"Enregistrement réussi.")
+
+            return HttpResponseRedirect(reverse('localisation:localisation'))
+    else:
+        return render(request, "localisation/ajouter_district.html", context)
+
+def details_district(request, id):
+
+    context = {
+        "titre": "Details du district",
+    }
+    district = District.objects.get(id = id)
+    if district != None:
+        return render(request,'localisation/modifier_district.html', context)
+
+    return render(request, "localisation/localisation.html", context)
+
+def modifier_district(request):
+
+    context = {
+        "titre": "Localisation",
+    }
+    if request.method == "POST":
+
+        district = District.objects.get(id = request.POST.get('id'))
+
+        if district != None:
+            district.code = request.POST.get("code")
+            district.libelle = request.POST.get("libelle")
+            district.save()
+
+            messages.success(request, "Modification réussie.")
+
+            return HttpResponseRedirect(reverse('localisation:localisation'))
+    else:
+        return render(request, "localisation/modifier_district.html", context)
+
+def supprimer_district(request, id):
+    district = District.objects.get(id=id)
+    district.delete()
+    messages.success(request, "Suppression réussie.")
+
+    return HttpResponseRedirect(reverse('localisation:localisation'))
+
+# Fin de la Gestion du district -------------------------------------
