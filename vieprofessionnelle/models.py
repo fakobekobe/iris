f7153770614b63d7e0717a27734464e9da50f804
Tano Fabien <kobejeanfabien@gmail.com>
2022-11-04 22:03:44 +0000

    vieprofessionnelle: Test de la relation many to many

--- vieprofessionnelle/models.py
+++ vieprofessionnelle/models.py
@@ -12,3 +12,43 @@ class Secteur(models.Model):
 
     def __str__(self):
         return self.secteur
+
+class SecteurActive(models.Model):
+    nom = models.CharField(max_length=250,null=False,blank=False)
+    date_adhesion = models.DateField(auto_now=True, verbose_name="Date d'adhésion")
+    numero_carte = models.CharField(max_length=250,null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+class Membre(models.Model):
+    nom = models.CharField(max_length=250, null=False,blank=False)
+
+    def __str__(self):
+        return self.nom
+
+class SecteurAgricole(SecteurActive):
+    speculation_agricole = models.IntegerField(verbose_name="Spéculation agricole")
+    superficie_en_culture = models.IntegerField(verbose_name="Superficie en culture")
+    superficie_en_production = models.IntegerField(verbose_name="Superficie en production")
+    gps_longitude = models.CharField(max_length=100, null=True, blank=True, verbose_name="GPS Longitude")
+    gps_latitude = models.CharField(max_length=100, null=True, blank=True, verbose_name="GPS Latitude")
+
+    membre = models.ManyToManyField(Membre)
+
+    def __str__(self):
+        return self.nom
+
+class SecteurInformel(SecteurActive):
+    metier = models.CharField(max_length=250, null=False, blank=False, verbose_name="Métier")
+
+    membre = models.ManyToManyField(Membre)
+
+    def __str__(self):
+        return self.metier
+
+class SecteurFemmeActive(SecteurActive):
+    personne_ressource = models.ManyToManyField(Membre)
+
+    def __str__(self):
+        return self.nom
